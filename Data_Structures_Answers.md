13508b5ba6b83d5d9f39ffdcf6498e712a6c5936
Hargobind Atwal <ethaipath@gmail.com>
2019-03-08 11:11:02 -0800

    best time solution (i think)

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,18 +1,22 @@
 Add your answers to the questions below.
 
 1. What is the runtime complexity of your `depth_first_for_each` method?
+n
 
 2. What is the space complexity of your `depth_first_for_each` function?
-
+log n
 3. What is the runtime complexity of your `breadth_first_for_each` method?
-
+n
 4. What is the space complexity of your `breadth_first_for_each` method?
-
+log n
 
 5. What is the runtime complexity of the provided code in `names.py`?
+n^2
 
 6. What is the space complexity of the provided code in `names.py`?
+O(1)
 
 7. What is the runtime complexity of your optimized code in `names.py`?
-
+(n log n)^2
 8. What is the space complexity of your optimized code in `names.py`?
+O(1)
--- names/names.py
+++ names/names.py
@@ -13,7 +13,36 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# STRECH
 
+
+# runtime: 0.061779022216796875 seconds
+def binary_search(arr, target):
+    if len(arr) == 0:
+        return -1 # empty array
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        middle = (high+low)//2
+        if arr[middle] < target:
+        # get RHS
+            low = middle+1
+        elif arr[middle] > target:
+        # get LHS
+            high = middle-1
+        else:
+            return True #middle
+    return False # not found
+
+names_2.sort()
+duplicates = []
+for name_1 in names_1:
+    if binary_search(names_2, name_1):
+        duplicates.append(name_1)
+
+
+'''
+# MVP
 #runtime: 0.23628687858581543 seconds
 # O( (log n) ^ 2)
 bst = BinarySearchTree(names_2.pop())
@@ -23,34 +52,6 @@ for name_2 in names_2:
 duplicates = []
 for name_1 in names_1:
     if bst.contains(name_1): 
-        duplicates.append(name_1)
-
-
-'''
-def binary_search(arr, target):
-    if len(arr) <= 0:
-        print('nope')
-        return False
-    print('finding ', target, ' on array', len(arr))
-    # high = high #len(arr) - 1
-    # low = low #0
-    high = len(arr) - 1
-    low = 0
-    middle = high//2#(high-low)//2
-    if arr[middle] == target:
-        return True
-    elif arr[middle] > target:
-        # binary_search(arr, middle+1, high, target)
-        binary_search(arr[middle+1:high], target)
-    else:
-        # binary_search(arr, low, middle-1, target)
-        binary_search(arr[low:middle-1], target)
-
-
-names_2.sort()
-duplicates = []
-for name_1 in names_1:
-    if binary_search(names_2, name_1):
         duplicates.append(name_1)
 '''
 
